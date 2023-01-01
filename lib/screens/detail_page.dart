9a749c588c1f93e48452ca5db49f09a758a7a9cf
Azzam Ubaidillah <azzam.ubaidillah05@gmail.com>
2023-01-01 23:41:43 +0700

    detail page init

---
+++ lib/screens/detail_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:game_bloc/model/data_model.dart';
+
+class DetailPage extends StatelessWidget {
+  final DataModel dataModel;
+  const DetailPage({super.key, required this.dataModel});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
