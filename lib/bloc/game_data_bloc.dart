e4aaf4af3bc1396ed27091a453f9ff9660973411
Azzam Ubaidillah <azzam.ubaidillah05@gmail.com>
2023-01-01 19:22:53 +0700

    bloc setup

--- lib/bloc/game_data_bloc.dart
+++ lib/bloc/game_data_bloc.dart
@@ -1,13 +1,25 @@
 import 'package:bloc/bloc.dart';
+import 'package:game_bloc/model/data_model.dart';
+import 'package:game_bloc/repo/games_repo.dart';
 import 'package:meta/meta.dart';
 
 part 'game_data_event.dart';
 part 'game_data_state.dart';
 
 class GameDataBloc extends Bloc<GameDataEvent, GameDataState> {
-  GameDataBloc() : super(GameDataInitial()) {
-    on<GameDataEvent>((event, emit) {
-      // TODO: implement event handler
+  final GamesRepo gamesRepo;
+
+  GameDataBloc(this.gamesRepo) : super(GameDataInitial()) {
+    on<GameDataEvent>((event, emit) async {
+      if (event is LoadGameData) {
+        emit(GameDataLoading());
+        List<DataModel>? dataModels = await gamesRepo.getGamesData();
+        if (dataModels != null) {
+          emit(GameDataLoaded(dataModels: dataModels));
+        } else {
+          emit(GameDataError(message: 'Error loading data'));
+        }
+      }
     });
   }
 }
--- lib/bloc/game_data_state.dart
+++ lib/bloc/game_data_state.dart
@@ -4,3 +4,17 @@ part of 'game_data_bloc.dart';
 abstract class GameDataState {}
 
 class GameDataInitial extends GameDataState {}
+
+class GameDataLoading extends GameDataState {}
+
+class GameDataLoaded extends GameDataState {
+  final List<DataModel> dataModels;
+  GameDataLoaded({required this.dataModels});
+}
+
+class GameDataError extends GameDataState {
+  final String message;
+  GameDataError({required this.message});
+} 
+
+// Path: lib\bloc\game_data_event.dart
